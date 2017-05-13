4fa803e2d9edd0ce647954de13ca481a89253298
darvelher2103 <darvelher@hotmail.com>
2017-05-13 11:58:01 -0500

    Listado de Personas

---
+++ app/src/main/java/com/example/cuc/personabasedatos/Datos.java
@@ -0,0 +1,48 @@
+package com.example.cuc.personabasedatos;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+
+/**
+ * Created by CUC on 13/05/2017.
+ */
+
+public class Datos {
+    public static ArrayList <Persona> traerPersonas(Context contexto){
+        ArrayList<Persona> personas = new ArrayList<>();
+
+        SQLiteDatabase db;
+        String sql, foto, cedula, nombre, apellido, sexo, pasatiempo;
+        Persona p;
+
+        //Abrir la Conexion de lectura
+
+        PersonasSqliteOpenHelper aux = new PersonasSqliteOpenHelper(contexto, "DBPersonas", null, 1);
+        db = aux.getReadableDatabase();
+
+        // Cursor
+
+        sql = "select * from Personas ";
+        Cursor c = db.rawQuery(sql, null);
+
+        if (c.moveToFirst()){
+            do {
+                foto = c.getString(0);
+                cedula = c.getString(1);
+                nombre = c.getString(2);
+                apellido = c.getString(3);
+                sexo = c.getString(4);
+                pasatiempo = c.getString(5);
+                p = new Persona(foto, cedula, nombre, apellido, sexo, pasatiempo);
+                personas.add(p);
+            }while(c.moveToNext());
+        }
+
+        db.close();
+
+        return personas;
+    }
+}
--- app/src/main/java/com/example/cuc/personabasedatos/listadoTabla.java
+++ app/src/main/java/com/example/cuc/personabasedatos/listadoTabla.java
@@ -2,12 +2,15 @@ package com.example.cuc.personabasedatos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TableLayout;
 
 public class listadoTabla extends AppCompatActivity {
+    private TableLayout tabla;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listado_tabla);
+        tabla = (TableLayout)findViewById(R.id.tblPersonas);
     }
 }
